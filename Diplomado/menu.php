90f011c8ae66b419230b3fe9761426959f09ef76
itzel <al221510887@gmail.com>
2019-09-04 15:37:33 -0500

    agregado de menu mobil

--- Diplomado/menu.php
+++ Diplomado/menu.php
@@ -10,7 +10,30 @@ else{
 }*/
 
 echo "
-
+<div class='mobile-menu-area'>
+  <div class='container'>
+      <div class='row'>
+          <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+              <div class='mobile-menu'>
+                  <nav id='dropdown'>
+                      <ul class='mobile-menu-nav'>
+                          <li><a data-toggle='collapse' data-target='#Charts' href='#'>Home</a>
+                              <ul class='collapse dropdown-header-top'>
+                                  <li><a href='index.html'>Dashboard One</a></li>
+                                  <li><a href='consulta_paciente.php'>Consulta Paciente</a></li>
+                                  <li><a href='atencion_medica.php'>Atencion Medica</a></li>
+                                  <li><a href='resultados.php'>Resultados</a></li>
+                                  <li><a href='analytics.html'>Analytics</a></li>
+                                  <li><a href='widgets.html'>Widgets</a></li>
+                              </ul>
+                          </li>
+                      </ul>
+                  </nav>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
 <!-- Mobile Menu end -->
 <!-- Main Menu area start-->
 <div class='main-menu-area mg-tb-40'>
@@ -18,12 +41,7 @@ echo "
         <div class='row'>
             <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
                 <ul class='nav nav-tabs notika-menu-wrap menu-it-icon-pro'>
-
-
-
-";
-
-                echo "     <li><a  href='alta_paciente.php'><i class='notika-icon notika-form'></i> Alta Paciente</a>
+                 <li><a  href='alta_paciente.php'><i class='notika-icon notika-form'></i> Alta Paciente</a>
                     </li>
                     <li><a  href='consulta_paciente.php'><i class='notika-icon notika-form'></i>Conuslta Alta   Pacientes</a>
                       </li>
@@ -31,19 +49,7 @@ echo "
                     </li>
                     <li><a href='resultados.php'><i class='notika-icon notika-form'></i> Resultado Patologia</a>
                     </li>
-
-
-
-
-
-
                 </ul>
-
-
-
-
-
-
             </div>
         </div>
     </div>
