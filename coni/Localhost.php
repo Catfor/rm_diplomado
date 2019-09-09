eb494007e206e90a7a26e1fb3c0182184b8f44b1
Catfor <calv.catfor@gmail.com>
2019-09-09 09:34:27 -0500

    [FIX] Conexion a BD

--- coni/Localhost.php
+++ coni/Localhost.php
@@ -7,15 +7,4 @@ if(mysqli_connect_errno()){
   exit();
 }
  ?>
-/*
- <?php
 
- $mysqliL = new mysqli('localhost', 'root', '', 'diplomado',3306);
- mysqli_set_charset($mysqliL, 'utf8');
- //mysqli_query("SET NAMES 'utf8'");
- if(mysqli_connect_errno()){
-   echo 'Conexion Fallida : ', mysqli_connect_error();
-   exit();
- }
-
-  ?>*/
