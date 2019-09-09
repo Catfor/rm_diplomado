029f18b4ccb257914fec2c89c99bc5d826867f8a
Catfor <calv.catfor@gmail.com>
2019-09-09 09:25:12 -0500

    [MOD] codificación para apuntar a la DB del servidor

--- coni/Localhost.php
+++ coni/Localhost.php
@@ -1,5 +1,4 @@
 <?php
-/*
 $mysqliL = new mysqli('74.208.183.28', 'apps', 'apps1', 'diplomado',3307);
 mysqli_set_charset($mysqliL, 'utf8');
 //mysqli_query("SET NAMES 'utf8'");
@@ -7,8 +6,8 @@ if(mysqli_connect_errno()){
   echo 'Conexion Fallida : ', mysqli_connect_error();
   exit();
 }
-*/
  ?>
+/*
  <?php
 
  $mysqliL = new mysqli('localhost', 'root', '', 'diplomado',3306);
@@ -19,4 +18,4 @@ if(mysqli_connect_errno()){
    exit();
  }
 
-  ?>
+  ?>*/
